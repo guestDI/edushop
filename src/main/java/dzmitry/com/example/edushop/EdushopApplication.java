06eaafdd9240e54bbdf153382830ead5dbad5d8c
Dzmitry Ihnatovich <ignatovich.dm@gmail.com>
2017-09-26 13:15:44 +0300

    Initial commit

---
+++ src/main/java/dzmitry/com/example/edushop/EdushopApplication.java
@@ -0,0 +1,12 @@
+package dzmitry.com.example.edushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdushopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdushopApplication.class, args);
+	}
+}
---
+++ src/test/java/dzmitry/com/example/edushop/EdushopApplicationTests.java
@@ -0,0 +1,16 @@
+package dzmitry.com.example.edushop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdushopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
